0ea2b4286d8b59e38027490ddc591a49ddfe968d
valeria <valeriaososal@gmail.com>
2020-08-13 09:05:42 -0500

    add button created, buttons looks when hover changed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import SideBar from './views/components/SideBar';
 import AddForm from './views/components/AddForm';
+import AddButton from './views/components/AddButton'
 
 function App() {
   return (
     <div className='App'>
       <SideBar />
       <AddForm />
+      <AddButton />
     </div>
   );
 }
---
+++ src/views/components/AddButton/AddButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './AddButton.css';
+
+const AddButton = (props) => {
+    return <button className='AddButton'><i class="fas fa-plus"></i></button>;
+}
+
+export default AddButton;
