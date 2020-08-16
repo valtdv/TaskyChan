d9ffcd50efbfcec089039c6522ae02a9fe0382d7
valeria <valeriaososal@gmail.com>
2020-08-15 22:27:19 -0500

    footer created

---
+++ src/views/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import './Footer.css'
+
+class Footer extends Component {
+    render() {
+        return <footer className='Footer'></footer>
+    }
+}
+
+export default Footer;
