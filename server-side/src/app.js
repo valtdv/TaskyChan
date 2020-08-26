c52b470055771521d3e5206ed2ac1443bdbd00d4
VictorB <victormbp2@gmail.com>
2020-08-26 09:55:36 -0500

    Configuración inicial del servidor

---
+++ server-side/src/app.js
@@ -0,0 +1,17 @@
+//app.js es el encargado de definir el servidor 
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//settings
+app.set('port', process.env.PORT || 5000);
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//rutas
+//app.use('/api/users', require('./routes/users'));
+
+module.exports = app;
